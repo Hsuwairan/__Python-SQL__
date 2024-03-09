48f27cc5d9f6273c8c56807c215da46c56211ea0
Hsuwairan <151415013+Hsuwairan@users.noreply.github.com>
2024-03-09 10:45:35 +0800

    modify

--- sql/Lesson5_2.sql
+++ sql/Lesson5_2.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS 聯絡人
-DROP TABLE IF EXISTS 客戶
+DROP TABLE IF EXISTS 聯絡人;
+DROP TABLE IF EXISTS 客戶;
 
 CREATE TABLE IF NOT EXISTS 客戶(
 	客戶_id SERIAL,
@@ -17,6 +17,7 @@ CREATE TABLE IF NOT EXISTS 聯絡人(
 	郵件 VARCHAR(100),
 	PRIMARY KEY(聯絡人id),
 	FOREIGN KEY(客戶id) REFERENCES 客戶(客戶_id)
+	ON DELETE SET NULL
 );
 
 INSERT INTO 客戶(客戶名稱)
@@ -27,3 +28,9 @@ INSERT INTO 聯絡人(客戶id,聯絡人姓名,電話,郵件)
 VALUES(1,'james','02-29834762','789456@gmail.com'),
 	  (2,'wairan','02-22592140','James_Hsu@gmail.com'),
 	  (2,'1234','02-1234567582','1234@@gmail.com');
+	  
+DELETE FROM 客戶
+WHERE 客戶_id = 1
+
+SELECT * FROM 客戶;
+SELECT * FROM 聯絡人;
